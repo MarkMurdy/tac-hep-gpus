960653aa0b8e5e23139275ccc02d9cc94c7133ef
mmurdy requested-by-Charis <mmurdy@g38n02.hep.wisc.edu>
2025-10-26 22:58:43 -0500

    Added week 4 assignment

--- week4/dot_product.cu
+++ week4/dot_product.cu
@@ -26,6 +26,10 @@ __global__ void dot_product(const int *A, const int *B, int *C, int N) {
 
 	// FIXME
 	// Use atomicAdd	
+	int idx = blockDim.x*blockIdx.x + threadIdx.x;
+	// int product = A[idx] * B[idx];
+	if (idx < N)
+		atomicAdd(C, A[idx] * B[idx]);
 }
 
 
@@ -47,22 +51,42 @@ int main() {
 
 
 	// Allocate device memory 
+	cudaMalloc(&d_A, DSIZE*sizeof(int));
+	cudaMalloc(&d_B, DSIZE*sizeof(int));
+	cudaMalloc(&d_C, sizeof(int));
 	
 	// Check memory allocation for errors
+	cudaCheckErrors();
 
 	// Copy the matrices on GPU
+	cudaMemcpy(d_A, h_A, DSIZE*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, h_B, DSIZE*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_C, h_C, sizeof(int), cudaMemcpyHostToDevice);
 	
 	// Check memory copy for errors
+	cudaCheckErrors();
 
 	// Define block/grid dimentions and launch kernel
+	int grid_size = (DSIZE + BLOCK_SIZE - 1) / BLOCK_SIZE;
+	dot_product<<<grid_size, BLOCK_SIZE>>>(d_A, d_B, d_C, DSIZE);
 	
 	// Copy results back to host
+	cudaMemcpy(h_C, d_C, sizeof(int), cudaMemcpyDeviceToHost);
 	
     // Check copy for errors
+	cudaCheckErrors();
 
 	// Verify result
+	int corr = a*b*DSIZE;
+	printf("Correct answer is %d, computed answer is %d\n", corr, *h_C);
 
 	// Free allocated memory
+	free(h_A);
+	free(h_B);
+	free(h_C);
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
 	
 	return 0;
 
